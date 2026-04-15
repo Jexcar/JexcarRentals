87522c0a20ae507bd1a050a81fa8405405de11f8
Jexcar <jess@jexcar.com>
2026-04-15 15:44:53 -0700

    Create home page app/page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Welcome to Jexcar Rentals</title>
+</head>
+<body>
+    <h1>Welcome to Jexcar Rentals!</h1>
+    <p>Your one-stop shop for amazing car rentals.</p>
+    <a href="/trailers">Go to Trailers Page</a>
+</body>
+</html>
